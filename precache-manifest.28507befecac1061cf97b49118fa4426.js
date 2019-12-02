b3f9030ecead7306acfca272f69c00f882522f1c
NinaKWelch <ninakwelch@gmail.com>
2019-12-02 22:11:48 +0000

    Updates

---
+++ precache-manifest.28507befecac1061cf97b49118fa4426.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ef0433a7468cb450e113552b86e0081",
+    "url": "/ninas-neighborhood-map/index.html"
+  },
+  {
+    "revision": "25d75429fd00fe6ca996",
+    "url": "/ninas-neighborhood-map/static/css/main.1ce29dac.chunk.css"
+  },
+  {
+    "revision": "89c1568cc3531bb0005b",
+    "url": "/ninas-neighborhood-map/static/js/2.ed62f01a.chunk.js"
+  },
+  {
+    "revision": "25d75429fd00fe6ca996",
+    "url": "/ninas-neighborhood-map/static/js/main.096c2aac.chunk.js"
+  },
+  {
+    "revision": "c44f60950498cbe1c07e",
+    "url": "/ninas-neighborhood-map/static/js/runtime-main.dc2bfaf9.js"
+  },
+  {
+    "revision": "2d00fbc711a9c6b81f3af979f51f3b4a",
+    "url": "/ninas-neighborhood-map/static/media/defaultImg.2d00fbc7.jpg"
+  }
+]);
