edb6e71abf3685a0649528f77dd022ebf8a57eae
NinaKWelch <ninakwelch@gmail.com>
2019-12-03 11:29:27 +0000

    Updates

--- precache-manifest.68e1185c552a555952a9a9d2483afe44.js
+++ precache-manifest.68e1185c552a555952a9a9d2483afe44.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "592838f84deb7bb9fdae5a066e43a5b9",
+    "revision": "127609b9d5cb22f1ae4c88bd4f2d015b",
     "url": "/ninas-neighborhood-map/index.html"
   },
   {
-    "revision": "1af37ad498060e0c9116",
-    "url": "/ninas-neighborhood-map/static/css/main.1940cfe2.chunk.css"
+    "revision": "43bb22130b2b8e66be6e",
+    "url": "/ninas-neighborhood-map/static/css/main.e2cc8392.chunk.css"
   },
   {
     "revision": "89c1568cc3531bb0005b",
     "url": "/ninas-neighborhood-map/static/js/2.ed62f01a.chunk.js"
   },
   {
-    "revision": "1af37ad498060e0c9116",
-    "url": "/ninas-neighborhood-map/static/js/main.9fc2cdcb.chunk.js"
+    "revision": "43bb22130b2b8e66be6e",
+    "url": "/ninas-neighborhood-map/static/js/main.e2350a90.chunk.js"
   },
   {
     "revision": "c44f60950498cbe1c07e",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ninas-neighborhood-map/precache-manifest.b5c61d6956028e45437961c4553a82e2.js"
+  "/ninas-neighborhood-map/precache-manifest.68e1185c552a555952a9a9d2483afe44.js"
 );
 
 self.addEventListener('message', (event) => {
